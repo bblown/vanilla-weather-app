f0e31e25c4d6a581ddb4fe6f6918297261825779
bblown <bblown08@gmail.com>
2022-09-23 12:18:57 +0100

    Added icon

--- src/app.js
+++ src/app.js
@@ -10,12 +10,14 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
+    console.log(response.data)
     let temperatureElement = document.querySelector("#temperature");
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
     let humidityElement = document.querySelector("#humidity");
     let windElement = document.querySelector("#wind");
     let dateElement = document.querySelector("#date");
+    let iconElement = document.querySelector("#icon");
 
     temperatureElement.innerHTML =Math.round(response.data.main.temp);
     cityElement.innerHTML = (response.data.name);
@@ -23,10 +25,13 @@ function displayTemperature(response) {
     humidityElement.innerHTML = (response.data.main.humidity);
     windElement.innerHTML = Math.round(response.data.wind.speed);
     dateElement.innerHTML = formatDate(response.data.dt *1000);
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 
 }
 
 let apiKey = "6cc4cc2b1a264461f62f57395b11f08c";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Lisabon&appid=${apiKey}&units=metric`;
- 
+let city = "Lisbon";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+
 axios.get(apiUrl).then(displayTemperature);
