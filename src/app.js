eb2fe0f80fd66f167f08a1e13bc5b50c85254cf6
bblown <bblown08@gmail.com>
2022-09-26 17:46:21 +0100

    JS Templete

--- src/app.js
+++ src/app.js
@@ -8,6 +8,28 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes} `;
 
 }
+function displayForecast() {
+    let forecastElement = document.querySelector("#forecast");
+    let forecastHtml = `<div class="row">`;
+    let days = ["Thu","Fri","Sat"];
+    days.forEach(function(day) {
+    forecastHtml = forecastHtml +`                     
+                        <div class="col-2">
+                            <div class="weather-forecast-date">
+                                ${day}
+                            </div>
+                            <img src="http://openweathermap.org/img/wn/02d@2x.png" alt="" width="36" />
+                            <div class="weather-forecast-temperature">
+                                <span class="weather-forecast-temperature-max">18° </span>
+                                <span class="weather-forecast-temperature-min">12° </span>
+                            </div>
+                        </div>
+                `;
+    });
+    forecastHtml = forecastHtml + `</div>`;
+    forecastElement.innerHTML = forecastHtml;
+            
+}
 
 function displayTemperature(response) {
     console.log(response.data)
@@ -60,6 +82,7 @@ function displaycelsiusTemperature(event) {
 }
 
 let celsiusTemperature = null;
+displayForecast();
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit",handleSubmit);
@@ -71,3 +94,4 @@ let celsiustLink = document.querySelector("#celsiusLink");
 celsiustLink.addEventListener("click", displaycelsiusTemperature);
 
 search("New York");
+displayForecast();
